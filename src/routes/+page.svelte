a4746b3ccf04e3e979df4219e08ad1f23f4618dd
Nat Alison <1278991+tesseralis@users.noreply.github.com>
2025-06-25 22:30:31 -0700

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,100 @@
+<script lang="ts">
+	import Handler from './Handler.svelte';
+
+	let qBeads = $state<Record<string, any>>({});
+	function getAccel(id: string) {
+		return qBeads[id]?.accel;
+	}
+
+	// TODO affect phi and theta
+	function setLightByAngle(id: string, phi: number, theta: number, color: string) {
+		qBeads[id].color = color;
+	}
+	const api = { getAccel, setLightByAngle };
+	const apiArg = `{${Object.keys(api).join(',')}}`;
+
+	let idCounter = $state(0);
+	function connectQBead() {
+		const id = (idCounter++).toString();
+		qBeads[id] = {
+			accel: { x: 0, y: 0, z: 0 },
+			lightIndex: [0, 0],
+			color: 'black'
+		};
+
+		setInterval(() => {
+			qBeads[id].accel = {
+				x: clamp((qBeads[id].x ?? 0) + (Math.random() - 0.5), -1, 1),
+				y: clamp((qBeads[id].y ?? 0) + (Math.random() - 0.5), -1, 1),
+				z: clamp((qBeads[id].z ?? 0) + (Math.random() - 0.5), -1, 1)
+			};
+		}, 150);
+
+		function clamp(n: number, min: number, max: number) {
+			return Math.max(min, Math.min(max, n));
+		}
+	}
+</script>
+
+<div>
+	<details>
+		<summary>API</summary>
+		<ul>
+			<li>
+				<code>getAccel(id: string)</code> - gets the acceleration data of the QBead a
+				<code>{`{x, y, z}`}</code> object.
+			</li>
+			<li>
+				<code> setLightByAngle(id: string, phi: number, theta: number, color: string) </code> - set
+				the color of the QBead with the given angle and color string. Any
+				<a href="https://developer.mozilla.org/en-US/docs/Web/CSS/color_value" target="blank">
+					color accepted by CSS
+				</a> is available.
+			</li>
+		</ul>
+	</details>
+	<button onclick={connectQBead}>+ Add QBead</button>
+	<div class="qBeadPanels">
+		{#each Object.entries(qBeads) as [id, qbead]}
+			<section class="qBeadPanel">
+				<p>{id}</p>
+				<button onclick={() => qbead.onTap?.(api)}>Tap</button>
+				<p>
+					x: {qbead.accel.x.toFixed(3)}, y: {qbead.accel.y.toFixed(3)}, z: {qbead.accel.z.toFixed(
+						3
+					)}
+				</p>
+				<!-- TODO replace with actual visualizer -->
+				<svg class="visualizer" viewBox="-50 -50 100 100">
+					<circle r="40" stroke="black" fill={qbead.color} />
+				</svg>
+				<Handler
+					title="onAccelUpdate"
+					onapply={(text) => {
+						qBeads[id].onAccelUpdate = new Function(apiArg, text);
+					}}
+				/>
+				<Handler
+					title="onTap"
+					onapply={(text) => {
+						qBeads[id].onTap = new Function(apiArg, text);
+					}}
+				/>
+			</section>
+		{/each}
+	</div>
+</div>
+
+<style>
+	.qBeadPanels {
+		display: grid;
+		grid-template-columns: repeat(3, 1fr);
+		gap: 1rem;
+	}
+
+	.qBeadPanel {
+		padding: 1rem;
+		border: 1px solid black;
+		border-radius: 10px;
+	}
+</style>
---
+++ src/routes/Handler.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	interface Props {
+		title: string;
+		onapply(text: string): void;
+	}
+	const { title, onapply }: Props = $props();
+	let text = $state('');
+</script>
+
+<div class="function">
+	<label
+		><h2>{title}</h2>
+		<textarea bind:value={text}></textarea>
+	</label>
+	<button onclick={() => onapply(text)}>Apply</button>
+</div>
+
+<style>
+	.function {
+		display: flex;
+		flex-direction: column;
+	}
+
+	.function textarea {
+		width: 100%;
+		height: 99px;
+	}
+</style>
