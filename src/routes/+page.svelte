7037492c48e446f81d6c5a90faafd569b2bc2636
Nat Alison <1278991+tesseralis@users.noreply.github.com>
2025-07-02 23:35:26 -0700

    Move docs to its own component.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,7 @@
 	import 'core-js/features/symbol/observable.js';
 	import BlochSphere from './BlochSphere.svelte';
 	import { BlochVector } from '@qbead/bloch-sphere';
+	import Docs from './Docs.svelte';
 
 	interface QBeadState {
 		// accel: BlochVector;
@@ -27,7 +28,6 @@
 	const api = { getAccel, setLightByAngle };
 	const apiArg = `{${Object.keys(api).join(',')}}`;
 
-	// let idCounter = $state(0);
 	function connectQBead() {
 		const id = uuid();
 		qBeads[id] = {
@@ -52,24 +52,7 @@
 </script>
 
 <div>
-	<details>
-		<summary>API</summary>
-		<ul>
-			<li>
-				<code>getAccel(id: string)</code> - gets the acceleration data of the QBead as a
-				<a href="https://qbead.gitbook.io/bloch-sphere/classes/blochvector">
-					<code>BlochVector</code>
-				</a>.
-			</li>
-			<li>
-				<code> setLightByAngle(id: string, phi: number, theta: number, color: string) </code> - set
-				the color of the QBead with the given angle and color string. Any
-				<a href="https://developer.mozilla.org/en-US/docs/Web/CSS/color_value" target="blank">
-					color accepted by CSS
-				</a> is available.
-			</li>
-		</ul>
-	</details>
+	<Docs />
 	<button onclick={connectQBead}>+ Add QBead</button>
 	<div class="qBeadPanels">
 		{#each Object.entries(qBeads) as [id, qbead]}
---
+++ src/routes/Docs.svelte
@@ -0,0 +1,18 @@
+<details>
+	<summary>API</summary>
+	<ul>
+		<li>
+			<code>getAccel(id: string)</code> - gets the acceleration data of the QBead as a
+			<a href="https://qbead.gitbook.io/bloch-sphere/classes/blochvector">
+				<code>BlochVector</code>
+			</a>.
+		</li>
+		<li>
+			<code> setLightByAngle(id: string, phi: number, theta: number, color: string) </code> - set
+			the color of the QBead with the given angle and color string. Any
+			<a href="https://developer.mozilla.org/en-US/docs/Web/CSS/color_value" target="blank">
+				color accepted by CSS
+			</a> is available.
+		</li>
+	</ul>
+</details>
