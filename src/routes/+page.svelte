65c2663632482e0f99fdacc26ea12b2518a6242a
Nat Alison <1278991+tesseralis@users.noreply.github.com>
2025-07-14 02:15:57 -0700

    Refactor the way QBeads are connected.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,9 @@
 <script lang="ts">
 	import 'core-js/features/symbol/observable.js';
-	import { BlochVector } from '@qbead/bloch-sphere';
 	import Docs from './Docs.svelte';
-	import { Color, type ColorRepresentation } from 'three';
 	import QBeadPanel from './QBeadPanel.svelte';
 	import lzString from 'lz-string';
-
-	interface QBeadState {
-		id: string;
-		name?: string;
-		loading?: boolean;
-		accel?: BlochVector;
-		sphereCoord?: BlochVector;
-		color?: ColorRepresentation;
-		sphCharacteristic?: any;
-		accCharacteristic?: any;
-		colCharacteristic?: any;
-	}
+	import QBead from './QBead.svelte';
 
 	interface Handlers {
 		onTap?: string;
@@ -28,6 +15,7 @@
 		onAccelUpdate?: Function;
 	}
 
+	let qBeads = $state<QBead[]>([]);
 	const hash = window.location.hash.slice(1);
 	const handlerText = JSON.parse(lzString.decompressFromEncodedURIComponent(hash)) ?? [];
 	let handlerList = $state<Handlers[]>(handlerText);
@@ -35,53 +23,15 @@
 		handlerList.map((handlers, index) => {
 			const handlerFuncs: HandlerFuncs = {};
 			for (let funcName of ['onTap', 'onAccelUpdate'] as const) {
-				const func = new Function(apiArg, handlers[funcName] || '');
+				const func = new Function('{ qBeads, self }', handlers[funcName] || '');
 				handlerFuncs[funcName] = () => {
-					func({ ...api, self: index });
+					func({ qBeads, self: qBeads[index] });
 				};
 			}
 			return handlerFuncs;
 		})
 	);
-
-	let qBeads = $state<QBeadState[]>(handlerText.map(() => ({ id: '' })));
-
-	function getAccel(index: number) {
-		return qBeads[index]?.accel;
-	}
-
-	async function setLightByAngle(
-		index: number,
-		theta: number,
-		phi: number,
-		color: ColorRepresentation
-	) {
-		const qBead = qBeads[index];
-		if (!qBead) {
-			return;
-		}
-		qBead.sphereCoord = BlochVector.fromAngles(theta, phi);
-		qBead.color = color;
-		let newthetaphi = Uint8Array.of(
-			Math.floor((theta * 255) / Math.PI),
-			Math.floor((phi * 255) / (2 * Math.PI))
-		);
-		await qBead.sphCharacteristic.writeValue(newthetaphi);
-		const col = new Color(color);
-
-		let newColor = Uint8Array.of(
-			Math.floor(col.r * 255),
-			Math.floor(col.g * 255),
-			Math.floor(col.b * 255)
-		);
-		await qBead.colCharacteristic.writeValue(newColor);
-	}
-
-	const api = { getAccel, setLightByAngle };
-	const apiArg = `{${Object.keys(api).join(',')}, self }`;
-
 	function addPanel() {
-		qBeads.push({ id: '' });
 		handlerList.push({});
 	}
 </script>
@@ -91,12 +41,7 @@
 	<button onclick={addPanel}>+ Add Panel</button>
 	<div class="qBeadPanels">
 		{#each handlerList as _, i}
-			<QBeadPanel
-				index={i}
-				bind:qbead={qBeads[i]}
-				handlerFuncs={handlerFuncs[i]}
-				bind:handlerList
-			/>
+			<QBeadPanel index={i} handlerFuncs={handlerFuncs[i]} bind:qBeads bind:handlerList />
 		{/each}
 	</div>
 </div>
--- src/routes/QBeadPanel.svelte
+++ src/routes/QBeadPanel.svelte
@@ -1,73 +1,36 @@
 <script lang="ts">
 	import Handler from './Handler.svelte';
 	import BlochSphere from './BlochSphere.svelte';
-	import { BlochVector } from '@qbead/bloch-sphere';
 	import lzString from 'lz-string';
 	import { goto } from '$app/navigation';
+	import QBead from './QBead.svelte';
 
-	let { qbead = $bindable(), handlerList = $bindable([]), handlerFuncs, index } = $props();
-	async function connectQBead() {
-		let serviceUuid = 'e30c1fc6-359c-12be-2544-63d6aa088d45';
+	let { qBeads = $bindable(), handlerList = $bindable([]), handlerFuncs, index } = $props();
 
-		let accUuid = 'e30c1fc9-359c-12be-2544-63d6aa088d45';
-		let sphUuid = 'e30c1fc8-359c-12be-2544-63d6aa088d45';
-		let colUuid = 'e30c1fc7-359c-12be-2544-63d6aa088d45';
-
-		console.log('Requesting Bluetooth Device...');
-		let device = await (navigator as any).bluetooth.requestDevice({
-			filters: [{ services: [serviceUuid] }]
-		});
-		qbead = {
-			id: device.id,
-			name: device.name,
-			accel: BlochVector.from(0, 0, -1),
-			loading: true
-		};
-		console.log('Connecting to GATT Server...');
-		let server = await device.gatt.connect();
-
-		console.log('Getting Service...');
-		let service = await server.getPrimaryService(serviceUuid);
-
-		console.log('Getting Characteristics...');
-
-		let sphCharacteristic = await service.getCharacteristic(sphUuid);
-		let colCharacteristic = await service.getCharacteristic(colUuid);
-		let accCharacteristic = await service.getCharacteristic(accUuid);
-		await accCharacteristic.startNotifications();
-		console.log('> Notifications started');
-		accCharacteristic.addEventListener('characteristicvaluechanged', (event: any) => {
-			new Uint8Array(event.target.value.buffer).reverse();
-			let value = event.target.value;
-			qbead.accel = BlochVector.from(value.getFloat32(0), value.getFloat32(4), value.getFloat32(8));
-			handlerFuncs?.onAccelUpdate();
-		});
-
-		qbead = {
-			id: device.id,
-			name: device.name,
-			sphCharacteristic,
-			colCharacteristic,
-			accCharacteristic,
-			accel: BlochVector.from(0, 0, -1)
-		};
-	}
+	let loading = $state(false);
+	const qBead = $derived(qBeads[index]);
 </script>
 
 <section class="qBeadPanel">
-	{#if !qbead.id}
-		<button onclick={connectQBead}>Connect</button>
-	{:else if qbead.loading}
+	{#if !qBead}
+		<button
+			onclick={async () => {
+				loading = true;
+				qBeads[index] = await QBead.connect(handlerFuncs);
+				loading = false;
+			}}>Connect</button
+		>
+	{:else if loading}
 		<p>Loading...</p>
 	{:else}
-		<h3>{qbead.name}</h3>
-		<p>{qbead.id}</p>
+		<!-- <h3>{qbead.name}</h3>
+		<p>{qbead.id}</p> -->
 		<!-- The 'tap' hasn't been implemented in the QBead firmware, so mock it out here. -->
 		<button onclick={() => handlerFuncs.onTap?.()}>Tap</button>
 		<p>
-			x: {qbead.accel.x.toFixed(3)}, y: {qbead.accel.y.toFixed(3)}, z: {qbead.accel.z.toFixed(3)}
+			x: {qBead.accel.x.toFixed(3)}, y: {qBead.accel.y.toFixed(3)}, z: {qBead.accel.z.toFixed(3)}
 		</p>
-		<BlochSphere accel={qbead.accel} sphereCoord={qbead.sphereCoord} color={qbead.color} />
+		<BlochSphere accel={qBead.accel} sphereCoord={qBead.sphereCoord} color={qBead.color} />
 	{/if}
 	{#each ['onAccelUpdate', 'onTap'] as handlerName}
 		<Handler
