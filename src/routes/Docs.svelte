3c2a3a41599f8593ab642a2a7d20046a1497d88d
Nat Alison <1278991+tesseralis@users.noreply.github.com>
2025-07-14 02:44:54 -0700

    Update the API.

--- src/routes/Docs.svelte
+++ src/routes/Docs.svelte
@@ -2,20 +2,36 @@
 	<summary>API</summary>
 	<ul>
 		<li>
-			<code>self</code> - the id of the current QBead.
+			<code>self</code> - the current QBead.
 		</li>
 		<li>
-			<code>getAccel(id: string)</code> - gets the acceleration data of the QBead as a
+			<code>qBeads[i]</code> - the QBead at index i.
+		</li>
+	</ul>
+	<h2><code>class QBead</code></h2>
+	<ul>
+		<li>
+			<code>accel</code> - the acceleration data of the QBead as a
 			<a href="https://qbead.gitbook.io/bloch-sphere/classes/blochvector">
 				<code>BlochVector</code>
 			</a>.
 		</li>
 		<li>
-			<code> setLightByAngle(id: string, phi: number, theta: number, color: string) </code> - set
-			the color of the QBead with the given angle and color string. Any
-			<a href="https://developer.mozilla.org/en-US/docs/Web/CSS/color_value" target="blank">
-				color accepted by CSS
-			</a> is available.
+			<code>sphereCoords</code> - the currently highlighted spherical coordinate of the QBead as a
+			<a href="https://qbead.gitbook.io/bloch-sphere/classes/blochvector">
+				<code>BlochVector</code>
+			</a>.
+		</li>
+		<li>
+			<code>color</code> - the current color of the QBead.
+		</li>
+		<li>
+			<code>setSphereCoords(value: BlochVector)</code> - set the sphere coordinates of the QBead to the
+			given value.
+		</li>
+		<li>
+			<code>setColor(value: ColorRepresentation)</code> - set the color of the QBead to the given value.
+			Takes anything accepted as a Color in THREE.js.
 		</li>
 	</ul>
 </details>
